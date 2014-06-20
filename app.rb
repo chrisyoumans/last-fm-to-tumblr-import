1613a676e204e104f3b8876f7f5bc88906a6a17a
chrisyoumans <chris.youmans@careerbuilder.com>
2014-06-20 00:09:52 -0400

    DRY is dumb

--- app.rb
+++ app.rb
@@ -19,6 +19,8 @@ get '/lastfm/weeklytopfive/:username' do
   import = WeeklyTopFiveArtistImport.new
   import.retrieve_weekly_top_five("#{params[:username]}")
   import.artists.to_json
-  import.wrap_artists_in_html_for_tumblr
-  
+  body = import.wrap_artists_in_html_for_tumblr
+  tags = import.generate_tags
+  tum = TumblrMine.new
+  tum.post_to_tumblr('My Top 5 Artists (Week Ending ' + DateTime.now.strftime('%m/%d/%Y') + ')', body, tags)
 end
